79c3ce788ee5425729039dcaec6ed3c64694839c
Tori Hara <toriclstr@gmail.com>
2022-07-23 12:13:35 +0900

    大会終了時点で課金データを MySQL テーブルに保存するように

--- webapp/go/isuports.go
+++ webapp/go/isuports.go
@@ -600,6 +600,78 @@ type VisitHistorySummaryRow struct {
 }
 
 // 大会ごとの課金レポートを計算する
+func calculateBillingData(ctx context.Context, tenantDB dbOrTx, tenantID int64, competitonID string) (*BillingReport, error) {
+	comp, err := retrieveCompetition(ctx, tenantDB, competitonID)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieveCompetition: %w", err)
+	}
+
+	// ランキングにアクセスした参加者のIDを取得する
+	vhs := []VisitHistorySummaryRow{}
+	if err := adminDB.SelectContext(
+		ctx,
+		&vhs,
+		"SELECT player_id, MIN(created_at) AS min_created_at FROM visit_history WHERE tenant_id = ? AND competition_id = ? GROUP BY player_id",
+		tenantID,
+		comp.ID,
+	); err != nil && err != sql.ErrNoRows {
+		return nil, fmt.Errorf("error Select visit_history: tenantID=%d, competitionID=%s, %w", tenantID, comp.ID, err)
+	}
+	billingMap := map[string]string{}
+	for _, vh := range vhs {
+		// competition.finished_atよりもあとの場合は、終了後に訪問したとみなして大会開催内アクセス済みとみなさない
+		if comp.FinishedAt.Valid && comp.FinishedAt.Int64 < vh.MinCreatedAt {
+			continue
+		}
+		billingMap[vh.PlayerID] = "visitor"
+	}
+
+	// player_scoreを読んでいるときに更新が走ると不整合が起こるのでロックを取得する
+	fl, err := flockByTenantID(tenantID)
+	if err != nil {
+		return nil, fmt.Errorf("error flockByTenantID: %w", err)
+	}
+	defer fl.Close()
+
+	// スコアを登録した参加者のIDを取得する
+	scoredPlayerIDs := []string{}
+	if err := tenantDB.SelectContext(
+		ctx,
+		&scoredPlayerIDs,
+		"SELECT DISTINCT(player_id) FROM player_score WHERE tenant_id = ? AND competition_id = ?",
+		tenantID, comp.ID,
+	); err != nil && err != sql.ErrNoRows {
+		return nil, fmt.Errorf("error Select count player_score: tenantID=%d, competitionID=%s, %w", tenantID, competitonID, err)
+	}
+	for _, pid := range scoredPlayerIDs {
+		// スコアが登録されている参加者
+		billingMap[pid] = "player"
+	}
+
+	// 大会が終了している場合のみ請求金額が確定するので計算する
+	var playerCount, visitorCount int64
+	if comp.FinishedAt.Valid {
+		for _, category := range billingMap {
+			switch category {
+			case "player":
+				playerCount++
+			case "visitor":
+				visitorCount++
+			}
+		}
+	}
+	return &BillingReport{
+		CompetitionID:     comp.ID,
+		CompetitionTitle:  comp.Title,
+		PlayerCount:       playerCount,
+		VisitorCount:      visitorCount,
+		BillingPlayerYen:  100 * playerCount, // スコアを登録した参加者は100円
+		BillingVisitorYen: 10 * visitorCount, // ランキングを閲覧だけした(スコアを登録していない)参加者は10円
+		BillingYen:        100*playerCount + 10*visitorCount,
+	}, nil
+}
+
+// 大会ごとの課金レポートを取得する
 func billingReportByCompetition(ctx context.Context, tenantDB dbOrTx, tenantID int64, competitonID string) (*BillingReport, error) {
 	comp, err := retrieveCompetition(ctx, tenantDB, competitonID)
 	if err != nil {
@@ -670,6 +742,34 @@ func billingReportByCompetition(ctx context.Context, tenantDB dbOrTx, tenantID i
 		BillingVisitorYen: 10 * visitorCount, // ランキングを閲覧だけした(スコアを登録していない)参加者は10円
 		BillingYen:        100*playerCount + 10*visitorCount,
 	}, nil
+}
+
+// 計算済みの課金レポートを取得する
+func getBillingReportByCompetition(ctx context.Context, tenantDB dbOrTx, tenantID int64, competitonIDs []string) (*[]BillingReport, error) {
+
+	br := []BillingReport{}
+	if err := tenantDB.SelectContext(
+		ctx,
+		&br,
+		"SELECT * FROM billing WHERE tenant_id=? AND competitionID IN (?)",
+		tenantID, strings.Join(competitonIDs, ","),
+	); err != nil {
+		return nil, fmt.Errorf("error Select billing: %w", err)
+	}
+	brs := make([]BillingReport, 0, len(br))
+	for _, bill := range br {
+		brs = append(brs, BillingReport{
+			CompetitionID:     bill.CompetitionID,
+			CompetitionTitle:  bill.CompetitionTitle,
+			PlayerCount:       bill.PlayerCount,
+			VisitorCount:      bill.VisitorCount,
+			BillingPlayerYen:  bill.BillingPlayerYen,
+			BillingVisitorYen: bill.BillingPlayerYen,
+			BillingYen:        bill.BillingYen,
+		})
+	}
+
+	return &brs, nil
 }
 
 type TenantWithBilling struct {
@@ -749,11 +849,15 @@ func tenantsBillingHandler(c echo.Context) error {
 			); err != nil {
 				return fmt.Errorf("failed to Select competition: %w", err)
 			}
+			compIDs := make([]string, 0, len(cs))
 			for _, comp := range cs {
-				report, err := billingReportByCompetition(ctx, tenantDB, t.ID, comp.ID)
-				if err != nil {
-					return fmt.Errorf("failed to billingReportByCompetition: %w", err)
-				}
+				compIDs = append(compIDs, comp.ID)
+			}
+			reports, err := getBillingReportByCompetition(ctx, tenantDB, t.ID, compIDs)
+			if err != nil {
+				return fmt.Errorf("failed to getBillingReportByCompetition: %w", err)
+			}
+			for _, report := range *reports {
 				tb.BillingYen += report.BillingYen
 			}
 			tenantBillings = append(tenantBillings, tb)
@@ -1041,6 +1145,22 @@ func competitionFinishHandler(c echo.Context) error {
 			now, now, id, err,
 		)
 	}
+
+	bills, err := calculateBillingData(ctx, tenantDB, v.tenantID, id)
+	if err != nil {
+		return fmt.Errorf("error calculateBillingData: %w", err)
+	}
+	if _, err := adminDB.ExecContext(
+		ctx,
+		"INSERT INTO billing (tenant_id, competition_id, player_count, visitor_count, billing_player_yen, billing_visitor_yen, billing_yen, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+		v.tenantID, bills.CompetitionID, bills.PlayerCount, bills.VisitorCount, bills.BillingPlayerYen, bills.BillingVisitorYen, bills.BillingYen, now,
+	); err != nil {
+		return fmt.Errorf(
+			"error Insert competition: tenant_id=%d, competition_id=%s, player_count=%d, visitor_count=%d, billing_player_yen=%d, billing_visitor_yen=%d, billing_yen=%d, updatedAt=%d, %w",
+			v.tenantID, bills.CompetitionID, bills.PlayerCount, bills.VisitorCount, bills.BillingPlayerYen, bills.BillingVisitorYen, bills.BillingYen, now, err,
+		)
+	}
+
 	return c.JSON(http.StatusOK, SuccessResult{Status: true})
 }
 
--- webapp/sql/admin/10_schema.sql
+++ webapp/sql/admin/10_schema.sql
@@ -29,3 +29,16 @@ CREATE TABLE `visit_history` (
   `updated_at` BIGINT NOT NULL,
   INDEX `tenant_id_idx` (`tenant_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+CREATE TABLE billing (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  tenant_id BIGINT UNSIGNED NOT NULL,
+  competition_id VARCHAR(255) NOT NULL,
+  player_count BIGINT NOT NULL,
+  visitor_count BIGINT NOT NULL,
+  billing_player_yen BIGINT NOT NULL,
+  billing_visitor_yen BIGINT NOT NULL,
+  billing_yen BIGINT NOT NULL,
+  updated_at BIGINT NOT NULL,
+  INDEX `tenant_id_competition_id_idx` (`tenant_id`, `competition_id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
