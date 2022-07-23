b3d49ebcf8069d9cbb2b03b2627d62af25c92f0e
Songmu <y.songmu@gmail.com>
2022-07-23 12:13:22 +0900

    make rsync-dumpsql

--- webapp/sql/dump.sql.sh
+++ webapp/sql/dump.sql.sh
@@ -5,5 +5,5 @@
 set -ex
 datadir=initial_data
 for sql in $(ls $datadir | grep '.db'); do
-  ./webapp/sql/sqlite3-to-sql $datadir/$sql > $datadir/mysql/$sql.sql
+  ./webapp/sql/sqlite3-to-sql $datadir/$sql > $datadir/initial_data_mysql/$sql.sql
 done
